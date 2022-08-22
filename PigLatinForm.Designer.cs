1865c89eaae5a9444b0903d70e2a9ed58ca6d3f4
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-08-22 14:58:07 -0600

    Add project files.

---
+++ PigLatinForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace PigLatin
+{
+    partial class PigLatinForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.tanslateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(48, 32);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(488, 33);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel.Location = new System.Drawing.Point(48, 16);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(192, 16);
+            this.inputLabel.TabIndex = 1;
+            this.inputLabel.Text = "Input sentence to be translated:";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(40, 280);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(56, 16);
+            this.outputLabel.TabIndex = 3;
+            this.outputLabel.Text = "Results:";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(40, 295);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(488, 33);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // tanslateButton
+            // 
+            this.tanslateButton.Location = new System.Drawing.Point(104, 152);
+            this.tanslateButton.Name = "tanslateButton";
+            this.tanslateButton.Size = new System.Drawing.Size(368, 56);
+            this.tanslateButton.TabIndex = 4;
+            this.tanslateButton.Text = "Generate Pig Latin";
+            this.tanslateButton.UseVisualStyleBackColor = true;
+            // 
+            // PigLatinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.tanslateButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "PigLatinForm";
+            this.Text = "Pig Latin Translator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button tanslateButton;
+    }
+}
+
